b6595c59d82366f49350a7fb2ca5db1b918ed409
Myra James <Treesmellgood@gmail.com>
2021-01-11 20:27:10 -0500

    Initial Commit

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>It's time to practice coding</h1>
+        <h2>Welcome to my practice website!</h2>
+      </div>
+    );
+  }
+}
