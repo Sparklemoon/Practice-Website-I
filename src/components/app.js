886ca36f5a7c40929f99ee6c834e1cec00cc486b
Myra James <Treesmellgood@gmail.com>
2021-01-16 19:01:49 -0500

    Javascript for hamburger

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,2 @@
-import React, { Component } from 'react';
-
-export default class App extends Component {
-  render() {
-    return (
-      <div className='app'>
-        <h1>It's time to practice coding</h1>
-        <h2>Welcome to my practice website!</h2>
-      </div>
-    );
-  }
-}
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('navbar-menu')
