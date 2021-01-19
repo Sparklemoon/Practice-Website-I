9e03614e31ca1ac2715a9531295fa0b4413d9bd6
Myra James <Treesmellgood@gmail.com>
2021-01-19 13:22:32 -0500

    added hero section styles

--- src/components/app.js
+++ src/components/app.js
@@ -2,7 +2,7 @@ const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('navbar-menu')
 
 const mobileMenu = () => {
-  menu.classList.toggle(is-active);
+  menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
 };
 
