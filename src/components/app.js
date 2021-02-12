aa9e4b199ed931dcbc2bac8490d7f81a7fb71653
Myra James <Treesmellgood@gmail.com>
2021-02-11 19:48:31 -0500

    added highlight class with if else if conditional

--- src/components/app.js
+++ src/components/app.js
@@ -12,8 +12,30 @@ menu.addEventListener('click', mobileMenu);
 // show activ menu when scrolling
 
 const highlightMenu = () => {
-  const elem = document.querySelector('.highlight')
+  const element = document.querySelector('.highlight')
   const homeMenu = document.querySelector('#unicorns')
   const aboutMenu = document.querySelector('#dolphins')
   const servicesMenu = document.querySelector('#rainbow')
+  let scrollPosition = window.scrollY
+
+  //adds highlight class to menu items
+  if (window.innerWidth > 960 && scrollPosition < 600) {
+    unicornsMenu.classList.add('highlight')
+    dolphinsMenu.classList.remove('highlight')
+    return
+  } else if (window.innerWidth > 960 && scrollPosition < 1400 ) {
+    dolphinsMenu.classList.add('highlight')
+    unicornsMenu.classList.remove('highlight')
+    rainbowMenu.classList.remove('highlight')
+    return
+  } else if (window.innerWidth > 960 && scrollPosition < 2345 ) {
+    rainbowMenu.classList.add('highlight')
+    dolphinsMenu.classList.remove('highlight')
+    return
+  }
+
+  if((element && window.innerWidth < 960 && scrollPosition < 600) || element) {
+    elem.classList.remove('highlight')
+  }
+  
 }
