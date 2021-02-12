ae0de9fae48f56237ca2e17714b2a567182685c0
Myra James <Treesmellgood@gmail.com>
2021-02-11 19:26:06 -0500

    added some js for menu when scrolling

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('navbar-menu')
+const navLogo = document.querySelector('#navbar-logo')
 
 const mobileMenu = () => {
   menu.classList.toggle('is-active');
@@ -7,3 +8,12 @@ const mobileMenu = () => {
 };
 
 menu.addEventListener('click', mobileMenu);
+
+// show activ menu when scrolling
+
+const highlightMenu = () => {
+  const elem = document.querySelector('.highlight')
+  const homeMenu = document.querySelector('#unicorns')
+  const aboutMenu = document.querySelector('#dolphins')
+  const servicesMenu = document.querySelector('#rainbow')
+}
